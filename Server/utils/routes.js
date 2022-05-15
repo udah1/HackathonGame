ff4610963ddd86b65d9af4f4d7ab25e066483627
= <=>
2022-05-15 19:34:56 +0300

    m

--- Server/utils/routes.js
+++ Server/utils/routes.js
@@ -10,7 +10,7 @@ class Routes{
     appRoutes(){
         /* Getting the total room count and Available rooms to chat */
         this.app.get('/getRooms', (request, response) => {
-            response.status(200).json(this.data.rooms);
+            response.status(200).json(this.data.rooms); 
         });
         this.app.get('/categories', (request, response) => {
             response.status(200).json(Object.keys(this.categories));
