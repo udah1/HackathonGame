0c6723ea6170bf508691f2143f53b88a8bee6d30
Huri, Yehuda <Yehuda.Huri2@uscellular.com>
2022-05-15 20:39:09 +0300

    ignore

--- Server/utils/socket.js
+++ Server/utils/socket.js
@@ -1,16 +1,26 @@
 'use strict';
+const categories = require('../categories.json');
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 class Socket {
+
+    getAvailableRooms = () => {
+        let rooms = {};
+        console.log(this.data.rooms, 'rooms');
+        Object.keys(this.data.rooms).forEach(key => {
+            if (this.data.rooms[key].available) {
+                rooms[key] = this.data.rooms[key];
+            }
+        });
+        return rooms
+    };
 
     constructor(socket, data) {
         this.io = socket;
         this.data = data;
-        /* Win combination to check winner of the Game.*/
-        this.winCombination = [
-            [1, 2, 3], [4, 5, 6], [7, 8, 9],
-            [1, 4, 7], [2, 5, 8], [3, 6, 9],
-            [1, 5, 9], [7, 5, 3]
-        ];
     }
 
     socketEvents() {
@@ -27,26 +37,41 @@ class Socket {
              * In this Event user will create a new Room and can ask someone to join.
              */
             socket.on('create-room', (socketData) => {
-
+                console.log('create-room', socketData);
                 let id = 1;
                 let found = false;
                 while (!found) {
-                    found = Object.keys(this.data).every(key => key !== String(id));
+                    found = Object.keys(this.data.rooms).every(key => key !== String(id));
                     if (!found) {
                         id += 1;
                     }
                 }
                 const {user} = socketData;
-
-                this.data.rooms[id] ={
+                socket.join("room-" + id);
+                this.data.rooms[String(id)] = {
+                    id,
                     players: {},
-                    isFull: false
+                    available: true
                 };
-                this.data.rooms[id].players[user] = {points: 0};
-
+                this.data.rooms[id].players[user] = {user, points: 0};
+                console.log('room created', id);
                 IO.emit('room-created', {id});
-                socket.broadcast.emit( {
-                    rooms: this.data.rooms
+                const rooms = this.getAvailableRooms();
+                socket.broadcast.to("room-list").emit( 'rooms', {
+                    rooms: rooms
+                });
+            });
+            /*
+             * In this event will user can join the selected room
+             */
+            socket.on('room-list', () => {
+                console.log('room-list');
+                /* User Joining socket room */
+                socket.join("room-list");
+                /* Getting the room number from socket */
+                const rooms = this.getAvailableRooms();
+                IO.emit('rooms', {
+                    rooms
                 });
             });
 
@@ -54,64 +79,126 @@ class Socket {
              * In this event will user can join the selected room
              */
             socket.on('join-room', (socketData) => {
+                console.log('join-room', socketData);
                 const {roomNumber, user} = socketData;
                 const room = this.data.rooms[roomNumber];
-                if (room.isFull) {
-                    IO.emit('room-joined', 'FULL');
+                if (!room.available) {
+                    IO.emit('room-joined', {error: 'NOT-AVAILABLE'});
+                    return;
+                }
+
+                /* Getting the room number from socket */
+                if (room.players[user]) {
+                    IO.emit('room-joined', {error: 'USER-EXIST'});
                     return;
                 }
 
                 /* User Joining socket room */
                 socket.join("room-" + roomNumber);
-                this.data.allRooms = JSON.stringify({
-                    emptyRooms: emptyRooms,
-                    fullRooms: fullRooms
-                });
-                /* Getting the room number from socket */
-                const currentRoom = (Object.keys(IO.sockets.adapter.sids[socket.id]).filter(item => item !== socket.id)[0]).split('-')[1];
-                IO.emit('rooms-available', {
-                    'totalRoomCount': totalRoomCount,
-                    'fullRooms': fullRooms,
-                    'emptyRooms': emptyRooms
-                });
-                IO.sockets.in("room-" + roomNumber).emit('start-game', {
-                    'totalRoomCount': totalRoomCount,
-                    'fullRooms': fullRooms,
-                    'emptyRooms': emptyRooms,
-                    'roomNumber': currentRoom
-                });
+                room.players[user] = {user, points: 0};
+                if (room.players.length === 4) {
+                    room.available = false;
 
+                    const rooms = this.getAvailableRooms();
+                    IO.sockets.in('room-list').emit('rooms', {
+                        rooms
+                    });
+                }
+
+                IO.sockets.in("room-" + roomNumber).emit('room-joined', {
+                    room,
+                    new: user
+                });
             });
+
+
+            /*
+             * In this event will user can join the selected room
+             */
+            socket.on('start-game', (socketData) => {
+                console.log('start-game', socketData);
+                const {roomNumber, category} = socketData;
+                const room = this.data.rooms[roomNumber];
+
+                room.timeout = setTimeout(() => {
+                    room.gamePoints -= 10;
+
+                    const {sentence, reveledSentence} = room;
+                    const availableIndexToReveal = [];
+                    reveledSentence.split('').forEach((letter, index) => {
+                       if (letter === '*') {
+                           availableIndexToReveal.push(index);
+                       }
+                    });
+
+                    if (availableIndexToReveal.length === 0) {
+                        IO.sockets.in("room-" + roomNumber).emit('all-revealed');
+                        return;
+                    }
+                    const randomIndex= randomIntFromInterval(0, availableIndexToReveal.length - 1)
+                    const index = availableIndexToReveal[randomIndex];
+                    room.reveledSentence = reveledSentence.substr(0,index) + sentence[index] + reveledSentence.substr(index + 1);
+
+                    IO.sockets.in("room-" + roomNumber).emit('reveal-letter', {
+                        letters: [{ind: 5, val: 'א'}],
+                        score: room.gamePoints
+                    })
+                }, 10000);
+                const sentences = categories[category];
+                const sentence = sentences[index];
+                let toReveal;
+                let toRevealIndex;
+                do {
+                    toRevealIndex = randomIntFromInterval(0, sentence.length -1)
+                    toReveal = sentence[toRevealIndex];
+                } while (toReveal === ' ');
+                let reveledSentence = '';
+                sentence.split('').forEach((letter, index) => {
+                    if (letter === ' ' || index === toRevealIndex) {
+                        reveledSentence += letter;
+                    } else {
+                        reveledSentence += '*';
+                    }
+                });
+                room.sentence = sentence;
+                room.reveledSentence= reveledSentence;
+
+                IO.sockets.in("room-" + roomNumber).emit('init-board', {
+                    sentence: reveledSentence,
+                    score: room.gamePoints
+                })
+            });
+
 
             /*
              * This event will send played moves between the users
-             * Also Here we will calaculate the winner.
+             * Also Here we will calculate the winner.
              */
-            socket.on('send-move', (socketData) => {
-                const playedGameGrid = socketData.gameGrid;
-                const {user, roomNumber, playedClause} = socketData;
+            socket.on('player-guess', (socketData) => {
+                console.log('player-guess', socketData);
+                const {user, roomNumber, guess} = socketData;
                 const room = this.data.rooms[roomNumber];
                 const {generatedClause} = room;
                 let winner;
 
-                if (generatedClause.toLowerCase().indexOf(playedClause.toLowerCase()) >= 0) {
+                if (generatedClause.toLowerCase().indexOf(guess.toLowerCase()) >= 0) {
                     winner = user;
                     room.players[user].points += room.gamePoints;
                 }
 
                 if (!winner) {
-                    socket.broadcast.to("room-" + roomNumber).emit('receive-move', {
-                        'playedClause': socketData.playedClause,
+                    socket.broadcast.to("room-" + roomNumber).emit('receive-guess', {
+                        'guess': socketData.guess,
                         'winner': null
                     });
                 } else {
-                    IO.sockets.in("room-" + roomNumber).emit('receive-move', {
-                        'playedClause': generatedClause,
+                    clearTimeout(room.timeout);
+                    room.timeout = undefined;
+                    IO.sockets.in("room-" + roomNumber).emit('receive-guess', {
+                        'guess': generatedClause,
                         'winner': winner
                     });
                 }
-
-
             });
 
 
@@ -120,34 +207,8 @@ class Socket {
              * Here we will remove the room number from fullrooms array
              * And we will update teh Redis DB keys.
              */
-            socket.on('disconnecting', ()=> {
-                const rooms = Object.keys(socket.rooms);
-                const roomNumber = ( rooms[1] !== undefined && rooms[1] !== null) ? (rooms[1]).split('-')[1] : null;
-                if (rooms !== null) {
-                    Promise.all(['totalRoomCount', 'allRooms'].map(key => this.data[key])).then(values => {
-                        console.log(values);
-                        const allRooms = values[1];
-                        let totalRoomCount = values[0];
-                        let fullRooms = allRooms['fullRooms'];
-                        let emptyRooms = allRooms['emptyRooms'];
-
-                        let fullRoomsPos = fullRooms.indexOf(parseInt(roomNumber));
-                        if (fullRoomsPos > -1) {
-                            fullRooms.splice(fullRoomsPos, 1);
-                        }
-                        if (totalRoomCount > 1) {
-                            totalRoomCount--;
-                        } else {
-                            totalRoomCount = 1;
-                        }
-                        this.data.totalRoomCount = totalRoomCount;
-                        this.data.allRooms = JSON.stringify({
-                            emptyRooms: emptyRooms,
-                            fullRooms: fullRooms
-                        });
-                        IO.sockets.in("room-" + roomNumber).emit('room-disconnect', {id: socket.id});
-                    });
-                }//if ends
+            socket.on('disconnect', ()=> {
+                console.log(socket, 'socket');
             });
 
         });
